10c03f85fa71923d0d2d19d4cac79bd23649b3fb
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-01-24 12:43:45 +0000

    p1 original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,72 @@
-
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  float sum;
+  printf("enter the numbers\n");
+  scanf("%d%d",&num1,&num2);
+  sum= num1 + num2 ;
+  printf (" %d + %d = %f\n",num1,num2,sum );
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+
