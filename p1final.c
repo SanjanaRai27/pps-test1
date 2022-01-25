9e4cd1dcd1b1bb4feae3f01ad261f5f60384ab1f
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-01-25 09:04:47 +0000

    p1debuglog.txt

---
+++ p1final.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  float sum;
+  printf("enter the numbers\n");
+  scanf("%d%d",&num1,&num2);
+  sum= num1 + num2 ;
+  printf (" %d + %d = %f\n",num1,num2,sum );
+  return 0;
+}
